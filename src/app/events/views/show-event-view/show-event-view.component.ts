684604c4bcff79833df9334f1b90e7af812f90bc
Velka <contact@velka.dev>
2024-03-06 16:17:59 +0100

    Implement event show functionality

---
+++ src/app/events/views/show-event-view/show-event-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-event-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './show-event-view.component.html',
+})
+export class ShowEventViewComponent {
+
+}
